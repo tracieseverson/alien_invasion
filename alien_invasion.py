218057a945bc2ca2d4178d1dd77c0477108e7a65
Tracie <severson@usna.edu>
2022-10-14 11:28:08 -0400

    Create ship and draw on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,7 @@
-import pygame
 import sys
+import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -8,29 +10,32 @@ class AlienInvasion:
         """Initialze the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
         # Set the background color.
-        self.bg_color = (230, 230, 230)
+        #self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            #watch for keyboard and mouse events
+            # watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop
-            
-            self.screen.fill(self.bg_color)
 
-            #Make the most recently drawn screen visible.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
 
-if __name__ == '__main__':
-        #Make a game instance, and run the game
-    ai = AlienInvasion()
-    ai.run_game
 
+if __name__ == '__main__':
+    # Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
