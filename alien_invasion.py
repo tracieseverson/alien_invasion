2cced055933aaba2ab4b40b8ca26ce8ee26c79e5
Tracie <severson@usna.edu>
2022-10-18 12:20:00 -0400

    adjusting movement left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
 
@@ -16,12 +17,13 @@ class AlienInvasion:
         self.ship = Ship(self)
 
         # Set the background color.
-        #self.bg_color = (230, 230, 230)
+        # self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -29,6 +31,17 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
--- settings.py
+++ settings.py
@@ -4,6 +4,9 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings"""
         #Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (0, 0, 255)
+
+        #Ship settings
+        self.ship_speed = 1.5
