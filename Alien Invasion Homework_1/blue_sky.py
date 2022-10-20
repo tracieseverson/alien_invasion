65020c4425cfb78a01813887b7fdc5bb4f6392e7
Tracie <severson@usna.edu>
2022-10-20 11:43:04 -0400

    added game_character file

--- Alien Invasion Homework_1/blue_sky.py
+++ Alien Invasion Homework_1/blue_sky.py
@@ -5,14 +5,14 @@ import time
 # Initializing Pygame
 pygame.init()
 
-# Initializing surface
-surface = pygame.display.set_mode((400, 300))
+# Initializing screen
+screen = pygame.display.set_mode((400, 300))
 
 # Initialing RGB Color
 color = (0, 0, 255)
 
 # Changing surface color
-surface.fill(color)
+screen.fill(color)
 pygame.display.flip()
 
 time.sleep(10)
--- Alien Invasion Homework_1/game_character.py
+++ Alien Invasion Homework_1/game_character.py
@@ -1,17 +1,36 @@
+# Importing the libraries
 import pygame
+import time
 
 class NewImage:
     """A class to draw a new image on the screen"""
 
-    def __init__(self):
+    def __init__(self, screen):
         """Initialize the image and set its starting position"""
-        #self.screen = ai_game.screen
-        #self.screen_rect = ai_game.screen.get_rect()
-
-        self.image = pygame.image.load('close-portrait-gorgeous-maremma-sheepdog-260nw-1015627555.bmp')
+        self.screen = screen
+        self.image = pygame.image.load('dog.bmp')
         self.rect = self.image.get_rect()
+        self.rect.center = self.screen.get_rect().center
 
-        #self.rect.center = self.screen_rect.center
-
-    def blitme(self):
+    def draw(self):
         self.screen.blit(self.image, self.rect)
+
+
+# Initializing Pygame
+pygame.init()
+
+# Initializing screen
+screen = pygame.display.set_mode((400, 300))
+
+# Initialing RGB Color
+color = (0, 0, 255)
+
+# Changing surface color
+screen.fill(color)
+
+my_character = NewImage(screen)
+my_character.draw()
+
+pygame.display.flip()
+
+time.sleep(10)
