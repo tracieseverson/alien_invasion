443435981962515c3f020e73db68739c86a0ef28
Tracie <severson@usna.edu>
2022-10-19 20:49:19 -0400

    update ship

--- ship.py
+++ ship.py
@@ -12,7 +12,7 @@ class Ship:
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
-        self.rect = self.image.get_rect()
+        self.rect = self..get_rect()
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
         #self.rect.center = self.screen_rect.center
