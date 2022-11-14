2e3fefdf5d1590de8faf4b2d080634fde1253347
Tracie <severson@usna.edu>
2022-11-13 20:15:57 -0500

    finished game

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,9 +36,10 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -144,19 +145,32 @@ class AlienInvasion:
             print("Ship hit!!!")
             self._ship_hit()
 
+        #check if any aliens hit the bottom
+        self._check_aliens_bottom()
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen"""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                #treat like the ship got hit
+                self._ship_hit()
+                break
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien"""
-        #decrement ships_left
-        self.stats.ships_left = -1
-        #get rid of any remaining aliens and bullets
-        self.aliens.empty()
-        self.bullets.empty()
-        #create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
-        #pause
-        time.sleep(0.5)
+        if self.stats.ships_left > 0:
+            #decrement ships_left
+            self.stats.ships_left = -1
+            #get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+            #create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
+            #pause
+            time.sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _check_fleet_edge(self):
         """Respond if any aliens have reached an edge"""
--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,7 @@ class GameStats:
         """Initialize settings"""
         self.settings = ai_game.settings
         self.reset_stats()
+        self.game_active = True
 
     def reset_stats(self):
         """Initialize statstics that can change during the game"""
